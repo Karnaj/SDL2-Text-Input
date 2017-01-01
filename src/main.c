ffacc724ade577e75d8592148abe3108cf28f372
Karnaj <yoan.geran@hotmail.fr>
2016-12-31 23:01:01 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,74 +2,82 @@
 #include <stdio.h>
 #include <string.h>
 #define LEN_MAX 10
+#undef main
 
-SDL_Window *Init(void)
+int run(void);
+void copy(char *src, char *dst, int len);
+void copy(char *src, char *dst, int len)
 {
-   SDL_Window *window;
-   if(SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-		return NULL;
-	}
-	window = SDL_CreateWindow("Personnage 1.0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
-	if(NULL == window)
-		fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
-   return window;
+	while(len-- && (*dst++ = *src++));
 }
 
-int main(int argc, char* args[])
-{
-	SDL_Window *window = NULL;
-	int quit = SDL_FALSE;
-	char rep[LEN_MAX] = {0};
-	size_t len = 0, l;
 
+int run(void)
+{
+	char rep[LEN_MAX + 1] = {0};
+	size_t len = 0;
 	SDL_Event event;
-	if(window = Init() == NULL)
-      goto Quit;
-	SDL_StartTextInput();
+	SDL_bool quit = SDL_FALSE;
 	while(!quit)
 	{
+		SDL_bool has_type = SDL_FALSE;
 		SDL_WaitEvent(&event);
 		if(event.type == SDL_QUIT)
 			quit = SDL_TRUE;
 		else if( event.type == SDL_KEYDOWN)
 		{
-			if(event.key.keysym.sym == SDLK_ESCAPE)
-				quit = SDL_TRUE;
-			if(event.key.keysym.sym == SDLK_BACKSPACE && len > 0)
+			if(event.key.keysym.sym == SDLK_BACKSPACE && len)
 			{
 				rep[len - 1] = 0;
 				len--;
+				has_type = SDL_TRUE;
 			}
 			if(event.key.keysym.sym == SDLK_v && (SDL_GetModState() & KMOD_CTRL) && SDL_HasClipboardText())
 			{
 				char *tmp = SDL_GetClipboardText();
-				l = strlen(tmp);
-				strncpy(rep + len, tmp, LEN_MAX - 1 - len);
-            len += l;
-            if(len > len > LEN_MAX - 1)
-               len = LEN_MAX - 1;
+				size_t l = strlen(tmp);
+				size_t l_copy = len + l < LEN_MAX ? l : LEN_MAX - len;
+				strncpy(rep + len, tmp, l_copy);
+				len += l_copy;
 				SDL_free(tmp);
+				has_type = SDL_TRUE;
 			}
 			if(event.key.keysym.sym == SDLK_c && (SDL_GetModState() & KMOD_CTRL))
 				SDL_SetClipboardText(rep);
 		}
 		else if(event.type == SDL_TEXTINPUT)
 		{
-			l = strlen(event.text.text);
-			strncpy(rep + len, event.text.text, LEN_MAX - 1 - len);
-         len += l;
-			if(len > LEN_MAX - 1)
-            len = LEN_MAX - 1;
+			size_t l = strlen(event.text.text);
+			size_t l_copy = len + l < LEN_MAX ? l : LEN_MAX - len;
+			strncpy(rep + len, event.text.text, l_copy);
+			len += l_copy;
+			has_type = SDL_TRUE;
 		}
-      puts(rep);
+		if(has_type)
+			puts(rep);
 	}
-	SDL_StopTextInput();
-Quit:
-	if(window)
-		SDL_DestroyWindow(window);
-Fin:
-	SDL_Quit();
-	return 0;
+	return 1;
+}
+
+int main(void)
+{
+	int statut = EXIT_FAILURE;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+		fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+	else
+	{
+		SDL_Window *window = SDL_CreateWindow("Text Input", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
+		if(NULL == window)
+			fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+		else
+		{
+			statut = EXIT_SUCCESS;
+			SDL_StartTextInput();
+			run();
+			SDL_StopTextInput();
+			SDL_DestroyWindow(window);
+		}
+		SDL_Quit();
+	}
+	return statut;
 }
